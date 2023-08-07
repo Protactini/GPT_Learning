d3b3d2b886db162daf755cc4e9487a03928d79f1
danlin7L <sensationalldl@gmail.com>
2023-08-06 22:58:58 -0700

    Start

---
+++ pythontest.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import tiktoken #Wrod tokennizer from OpenAI
+
+# Setting tokeniser to a specific model  
+enc = tiktoken.get_encoding("cl100k_base")
+
+# Pparameters
+batch_size = 16 # how many independent sequences will be process in parallel
+block_size = 32 # what is the maximum context length for predictions?
+max_iters = 5000 # how many time does the block of learning will be repeated 
+eval_interval = 100
+learning_rate = 1e-3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 64
+n_head = 4
+n_layer = 4
+dropout = 0.0
+# ------------
+
+torch.manual_seed(1337)
+
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+
+# Using tiktoken to encode test and splite into train and test 
+data = torch.tensor(enc.encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
